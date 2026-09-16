b9dc96fad82e05c120236e499bcfa5cf65734d68
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-16 01:54:57 -0500

    Creacion del contadorConRemember

--- SEMANA4/LABORATORIO4/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
+++ SEMANA4/LABORATORIO4/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
@@ -5,34 +5,46 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.coronado.temperaturedisplay.ui.theme.TemperatureDisplayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TemperatureDisplayTheme {
-                    ContadorRoto()
-            }
+            ContadorConRemember()
         }
     }
 }
 
 @Composable
-fun ContadorRoto(){
-    var contador = 0 // Se reseta en cada recomposicion
-    Column{
-        Text("Contador: $contador")
-        Button(onClick = { contador++ }) {
+fun ContadorConRemember(){
+    // remember almacena al Int recomposiciones
+    var contador by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Contador: $contador",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {contador++}) {
             Text("Incrementar")
         }
     }
