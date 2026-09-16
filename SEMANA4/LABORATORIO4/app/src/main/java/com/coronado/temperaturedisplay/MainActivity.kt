d8c9c43e5389e81a52047ac57e6d0c1569d87c0e
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-16 02:02:47 -0500

    Creacion del proyecto y Data class Tarea

--- SEMANA4/LABORATORIO4/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
+++ SEMANA4/LABORATORIO4/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
@@ -4,48 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+data class Tarea(
+    val id: Int,
+    val nombre: String,
+    val completada: Boolean = false
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ContadorConRemember()
+            MaterialTheme {
+                PantallaTareas()
+            }
         }
     }
 }
 
-@Composable
-fun ContadorConRemember(){
-    // remember almacena al Int recomposiciones
-    var contador by remember { mutableStateOf(0) }
-
-    Column(
-        modifier = Modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Contador: $contador",
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = {contador++}) {
-            Text("Incrementar")
-        }
-    }
-}
