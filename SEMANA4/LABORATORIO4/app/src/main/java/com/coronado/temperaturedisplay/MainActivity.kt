23192f0de83fbd70c4a58292142011c2fc953582
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-16 02:03:56 -0500

    Creacion de ItemTarea

--- SEMANA4/LABORATORIO4/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
+++ SEMANA4/LABORATORIO4/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
@@ -31,3 +31,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun ItemTarea(
+    tarea: Tarea,
+    onEliminar: () -> Unit,
+    onCambiarEstado: (Boolean) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.weight(1f)
+            ) {
+                Checkbox(
+                    checked = tarea.completada,
+                    onCheckedChange = {
+                        onCambiarEstado(it)
+                    }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = tarea.nombre,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+            }
+            Button(onClick = onEliminar) {
+                Text("Eliminar")
+            }
+        }
+    }
+}
+
