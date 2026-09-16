9713873a1fcdcd4bef0f30e1c2e32fe7309f7246
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-16 03:12:43 -0500

    Agregar estado con remember y botones

--- SEMANA4/LABORATORIO04TAREA/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
+++ SEMANA4/LABORATORIO04TAREA/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.coronado.temperaturedisplay.ui.theme.TemperatureDisplayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +30,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TemperatureDisplay() {
+
+    var temperatura by remember { mutableStateOf(20) }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -35,5 +40,23 @@ fun TemperatureDisplay() {
             text = "Temperatura: 20 °C",
             fontSize = 28.sp
         )
+        Row {
+
+            Button(
+                onClick = {
+                    temperatura++
+                }
+            ) {
+                Text("Subir")
+            }
+
+            Button(
+                onClick = {
+                    temperatura--
+                }
+            ) {
+                Text("Bajar")
+            }
+        }
     }
 }
