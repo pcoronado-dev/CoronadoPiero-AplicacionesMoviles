81cfaa906f53d4f38b71962c9f2f2de133d16ac5
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-16 03:07:12 -0500

    Crear composable TemperatureDisplay

--- SEMANA4/LABORATORIO04TAREA/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
+++ SEMANA4/LABORATORIO04TAREA/app/src/main/java/com/coronado/temperaturedisplay/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.coronado.temperaturedisplay.ui.theme.TemperatureDisplayTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TemperatureDisplayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TemperatureDisplay()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TemperatureDisplayTheme {
-        Greeting("Android")
+fun TemperatureDisplay() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Temperatura: 20 °C",
+            fontSize = 28.sp
+        )
     }
 }
