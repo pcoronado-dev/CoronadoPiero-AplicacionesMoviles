c4ae90a2961723df019c666f9dbf9c4206304e87
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-22 23:39:56 -0500

    Se implemento AppNavigation con el NavHost y definición de rutas

--- SEMANA5/SEMANA5LAB/app/src/main/java/com/coronado/navegacion/MainActivity.kt
+++ SEMANA5/SEMANA5LAB/app/src/main/java/com/coronado/navegacion/MainActivity.kt
@@ -4,44 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.coronado.navegacion.ui.theme.NavegacionTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.coronado.navegacion.navigation.AppNavigation
+import com.coronado.navegacion.navigation.Screen
+import com.coronado.navegacion.screens.DetailScreen
+import com.coronado.navegacion.screens.HomeScreen
+import com.coronado.navegacion.screens.ListScreen
+import com.coronado.navegacion.screens.ProfileScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NavegacionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavegacionTheme {
-        Greeting("Android")
     }
 }
--- SEMANA5/SEMANA5LAB/app/src/main/java/com/coronado/navegacion/navigation/AppNavigation.kt
+++ SEMANA5/SEMANA5LAB/app/src/main/java/com/coronado/navegacion/navigation/AppNavigation.kt
@@ -1,2 +1,43 @@
 package com.coronado.navegacion.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.coronado.navegacion.screens.DetailScreen
+import com.coronado.navegacion.screens.HomeScreen
+import com.coronado.navegacion.screens.ListScreen
+import com.coronado.navegacion.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
